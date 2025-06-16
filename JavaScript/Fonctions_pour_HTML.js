88b9950984208cc14710a76987d08bd29c8f6125
Ahmed <ahmed.al-basri01@etu.umontpellier.fr>
2025-06-16 21:09:04 +0200

    cases plus grandes et mieux centrées

--- JavaScript/Fonctions_pour_HTML.js
+++ JavaScript/Fonctions_pour_HTML.js
@@ -505,6 +505,6 @@ function downloadCSV(array1, array2, filename = "data.csv") {
 
   //Agrandir taille case si nécessaire
   function resizeInput(item) {
-    item.style.width = Math.max(100, 7.86 * (2 + item.value.length)) + 'px';
+    item.style.width = Math.max(129, 7.86 * (3.3 + item.value.length)) + 'px';
 }
 
